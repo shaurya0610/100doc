b47892926d0ddf7e4eb9e1008fd6abc21f058d44
shaurya0610 <shaurya0610@gmail.com>
2022-11-25 17:27:09 +0530

    Calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,48 @@
+#Calculator
+
+from calculator_art import logo
+print(logo)
+
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+#Divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+    }
+
+def calculator():
+    num1 = float(input("What's the first number?: "))
+    
+    for symbol in operations:
+        print(symbol)
+        
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2}  = {answer}")
+        
+        if input("Type 'y' to continue calculating with {answer} or 'n' to start again: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+            
+calculator()
